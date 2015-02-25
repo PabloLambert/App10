289357a6cf93ba3c7bb9aede6a023002735f1454
Pablo Lambert <pablo.lambert@gmail.com>
2015-02-25 20:34:17 -0300

    Maps working, with Chilean default location.-

--- app/src/main/java/com/lambertsoft/app10/MapsActivity.java
+++ app/src/main/java/com/lambertsoft/app10/MapsActivity.java
@@ -3,14 +3,18 @@ package com.lambertsoft.app10;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+    private final LatLng HOME = new LatLng(-33.4311092,-70.5950772);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,6 +64,11 @@ public class MapsActivity extends FragmentActivity {
      * This should only be called once and when we are sure that {@link #mMap} is not null.
      */
     private void setUpMap() {
-        mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+
+        Marker Home = mMap.addMarker( new MarkerOptions().position(HOME).title("Home"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(HOME, 12));
+
+
+        //mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
     }
 }
